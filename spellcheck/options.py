29a322e1a928bbf0cae8a0e20e1d62e34e03de90
jcgy <17726751+jcgy@users.noreply.github.com>
2023-06-07 01:38:20 +0100

    Add logic to allow QT6 compatibility

---
+++ spellcheck/options.py
@@ -0,0 +1,55 @@
+# Import the main window object (mw) from aqt
+from aqt import mw
+from anki.hooks import addHook
+# Import the "show info" tool from utils.py
+from aqt.utils import showInfo, qconnect
+# Import all of the Qt GUI library
+from aqt.qt import *
+import glob
+import os
+# Import constants
+from .const import *
+
+# Options window
+class OptionsDialog(QDialog):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("Spellcheck Options")
+
+		# Dictionary selection
+		self.dictionary_combo = QComboBox()
+		# Get dictionary file names
+		dict_files = [os.path.basename(x) for x in glob.glob(f"{DICT_DIR}*")]
+		for i in dict_files:
+			self.dictionary_combo.addItem(f"{i}", f"{i}")
+		self.dictionary_combo.setCurrentIndex(self.dictionary_combo.findData(CONFIG["dictionary"]))
+
+		# Save buton
+		self.save_button = QPushButton("Save")
+		self.save_button.clicked.connect(self.save_options)
+
+		# Layout
+		layout = QVBoxLayout()
+		layout.addWidget(QLabel("Dictionary:"))
+		layout.addWidget(self.dictionary_combo)
+		layout.addWidget(self.save_button)
+		self.setLayout(layout)
+
+	# Save options
+	def save_options(self):
+		selected_dictionary = self.dictionary_combo.currentData()
+		CONFIG["dictionary"] = selected_dictionary
+		mw.addonManager.writeConfig(ADDON_NAME, CONFIG)
+		self.close()
+
+# Add the menu item to access the options dialog
+def show_options_dialog():
+	dialog = OptionsDialog()
+	dialog.exec()
+
+# Create a new menu item, "test"
+action = QAction("Spellcheck Options", mw)
+# set it to call testFunction when it's clicked
+qconnect(action.triggered, show_options_dialog)
+# And add it to the tools menu
+mw.form.menuTools.addAction(action)
---
+++ spellcheck/spellchecker.py
@@ -0,0 +1,56 @@
+from aqt.qt import *
+from aqt import mw
+from aqt.webview import AnkiWebView
+from anki.hooks import wrap, addHook
+from aqt import gui_hooks
+from anki.lang import _
+from functools import partial
+
+from .const import *
+
+QT6 = QT_VERSION_STR
+
+def replaceMisspelledWord(page, sug_word):
+    page.replaceMisspelledWord(sug_word)
+
+def onContextMenuEvent(web, menu):
+    # Add suggested words to context menu (middle click)
+    p=web._page.profile()
+
+    b=p.isSpellCheckEnabled()
+    menu.addSeparator()
+    a=menu.addAction(_("Spellcheck"))
+    a.setCheckable(True)
+    a.setChecked(b)
+    a.triggered.connect(lambda:p.setSpellCheckEnabled(not b))
+
+    if b:
+        firstAct=menu.actions()[0]
+        
+        # Handle QT API version differences here
+        if QT6:
+            data=web.lastContextMenuRequest()
+        else:
+            data=web._page.contextMenuData()
+
+        for sug_word in data.spellCheckerSuggestions():
+            a=menu.addAction(sug_word)
+            menu.insertAction(firstAct, a)
+            a.triggered.connect(partial(replaceMisspelledWord, web._page, sug_word))
+            # add in blod font
+            f=a.font()
+            f.setBold(True)
+            a.setFont(f)
+        menu.insertSeparator(firstAct)
+
+def setupBDIC(web, *args, **kwargs):
+    p=web._page.profile()
+    # Needs to be passed as list and without extentions .bdic
+    current_dictionary = [ CONFIG["dictionary"][:-5] ]
+    p.setSpellCheckLanguages(current_dictionary)
+    p.setSpellCheckEnabled(True)
+
+gui_hooks.editor_will_show_context_menu.append(onContextMenuEvent)
+gui_hooks.webview_will_show_context_menu.append(onContextMenuEvent)
+
+AnkiWebView.__init__=wrap(AnkiWebView.__init__, setupBDIC, "after")
